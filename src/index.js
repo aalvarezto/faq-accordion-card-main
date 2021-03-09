d80ec20d59f4cca263e458645233355374274bd1
Andres Alvarez <ndralvz@gmail.com>
2021-03-09 20:17:29 -0300

    layout completed

---
+++ src/index.js
@@ -0,0 +1,22 @@
+"use strict"
+
+const questions = document.querySelectorAll(".question")
+
+questions.forEach(question => {
+	const [button, answer] = question.children
+
+	button.addEventListener("click", () => {
+		closeAll(questions).except(answer)
+		answer.className = answer.className !== "visible" ? "visible" : "hidden"
+	})
+})
+
+function closeAll(x) {
+	return {
+		except: y =>
+			Array.from(x)
+				.map(question => question.children[1])
+				.filter(answer => answer.innerText !== y.innerText)
+				.forEach(answer => (answer.className = "hidden")),
+	}
+}
